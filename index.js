af91dfef2d91264ee237c0831a01d5d3ca8030f2
zJoaoPauloo <160547482+zJoaoPauloo@users.noreply.github.com>
2024-03-21 21:06:58 -0300

    criando tabela de funcionário
    e criando uma chave estrangeira para trazer o id do cargo com a tabela cargos

--- index.js
+++ index.js
@@ -30,3 +30,52 @@ const Cargo = conexao.define('cargos',{
 Cargo.sync({
     alter: true
 });
+//++++++++++++++++++++++++++++++++ usuario
+
+conexao.authenticate()
+    .then(() => {
+        console.log('conectado com sucesso.');
+
+    }).catch((erro)=>{
+        console.log('deu erro', erro);
+    });
+
+const funcionario = conexao.define('funcionario',{
+    codigo:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nome: {
+        type: Sequelize.STRING(150),
+        allowNull: false,
+
+    },
+    idade:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    cpf:{
+        type: Sequelize.STRING(11),
+        allowNull: false,
+    },
+    codigoCargo: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    }
+});
+
+funcionario.sync({
+    alter: true
+});
+
+funcionario.belongsTo(Cargo, { foreignKey: 'codigoCargo' });
+
+conexao.sync({ alter: true })
+    .then(() => {
+        console.log('Tabelas sincronizadas.');
+    })
+    .catch((erro)=>{
+        console.log('Erro ao sincronizar tabelas', erro);
+    });
